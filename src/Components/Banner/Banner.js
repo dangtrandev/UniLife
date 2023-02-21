18f4b85dce4d759b4d935d87f9bdec06a2efb31d
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-21 15:49:03 -0700

    fixing the button for searching option

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -15,9 +15,30 @@ export default function Banner() {
         </div>
         <div className="search_container">
             <div className="button_box">
-                <button id="option_button">Search by City</button>
-                <button id="option_button">Any Room</button>
-                <button>Find Homes</button>
+                {/* search by city button */}
+                <div class="dropdown-center">
+                    <button class="btn option-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Search By City
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Action two</a></li>
+                        <li><a class="dropdown-item" href="#">Action three</a></li>
+                    </ul>
+                </div>
+                {/*Choosing Room button*/}
+                <div class="dropdown-center">
+                    <button class="btn option-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Any Room 
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">1-2 people</a></li>
+                        <li><a class="dropdown-item" href="#">3-4 people</a></li>
+                        <li><a class="dropdown-item" href="#">5-6 people</a></li>
+                    </ul>
+                </div>
+                {/*Finds Home Button*/}
+                    <button>Find Homes</button>
             </div>
         </div>
     </div>
