efb75a82dc58378ed56ece713b2a703250dfe4e9
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-21 14:20:58 -0700

    adding bootstrap icon

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react'; 
+import "./NavBar.css"; 
 
 function NavBar() {
   return (
     <div className="nav_container"> 
-        <img src="Vector.jpg" alt="logo_brand" width ="40.85px" height="29.6px" />
+        <button>
+            <img src="Vector.jpg" alt="logo_brand" width ="40.85px" height="29.6px" />
+        </button>
 
+        <div className="right_button">
+            <a><i class="bi bi-heart"></i></a>
+        </div>
     </div>
   )
 }
