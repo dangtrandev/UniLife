75b54f8bacfe19e01f27cd6192c01b2405fa4582
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-21 16:21:11 -0700

    create the citilist components

---
+++ src/Components/CitiList/CitiList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CitiList() {
+  return (
+    <div className="list_container">
+        <h2>Student Accomodations in our top cities</h2>
+        <div className="city_list">
+
+        </div>
+    </div>
+  )
+}
