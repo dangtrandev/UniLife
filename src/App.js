36b48e95ae4a91791850eb2801595cc6f1e34c5e
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-21 14:15:16 -0700

    adding container for nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from 'react'; 
 import './App.css';
 import NavBar from './Components/NavBar/NavBar'; 
 
-import React from 'react'
-
 function App() {
   return (
-    <h1>Wellcome to the show</h1>
+    <NavBar />
   )
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className="nav_container"> 
+        <img src="Vector.jpg" alt="logo_brand" width ="40.85px" height="29.6px" />
+
+    </div>
   )
 }
 
