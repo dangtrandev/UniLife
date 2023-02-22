8b73dd029d09360ca57bcb03c26841929b63bf63
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-22 13:18:44 -0700

    insert feature logo inclusive

--- src/Components/Exclusive/Exclusive.js
+++ src/Components/Exclusive/Exclusive.js
@@ -7,20 +7,22 @@ export default function exclusive () {
         <div className="exclusive_box">
             <h3>Compare all inclusive student homes</h3>
             <div className="feature-container">
-                <div className="feature-item">
+                <div className="feature_item">
                     <img src="" alt="search_logo" />
                     <h4>Search</h4>
                     <p>Find your dream home in the perfect area near your university.</p>
                 </div>
-                <div className="feature-item">
+                <div className="feature_item">
                     <img src="" alt="compare_logo"/>
                     <h4>Compare</h4>
                     <p>Compare student accomodation to find the right home for you.</p>
                 </div>
-                <div className="feature-item">
+                <div className="feature_item">
                     <img src="" alt="bill_logo"/>
-                    <p>Bills are included in all rent prices.</p>
-                    <p>No hidden fees.</p>
+                    <h4>Bills Included</h4>
+                    <p>Bills are included in all rent prices.
+                        No hidden fees. 
+                    </p>
                 </div>
             </div>
         </div>
