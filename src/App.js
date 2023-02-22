511305a313ac2b697b556d1ebfb7aea95b35fa70
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-22 12:17:46 -0700

    create the exclusive box

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './Components/NavBar/NavBar'; 
 import Banner from './Components/Banner/Banner';
 import CitiList from './Components/CitiList/CitiList'; 
+import Exclusive from './Components/Exclusive/Exclusive';
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <Banner />
       <CitiList />
+      <Exclusive />
     </div>
      
   )
