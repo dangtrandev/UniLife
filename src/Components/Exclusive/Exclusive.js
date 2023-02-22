b0f612ee8dfb11db6cbb3e9f853a985e79466c3f
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-22 12:53:46 -0700

    define the value of layout

--- src/Components/Exclusive/Exclusive.js
+++ src/Components/Exclusive/Exclusive.js
@@ -6,8 +6,22 @@ export default function exclusive () {
     <div className="exclusive_container">
         <div className="exclusive_box">
             <h3>Compare all inclusive student homes</h3>
-            <div>
-
+            <div className="feature-container">
+                <div className="feature-item">
+                    <img src="" alt="search_logo" />
+                    <h4>Search</h4>
+                    <p>Find your dream home in the perfect area near your university.</p>
+                </div>
+                <div className="feature-item">
+                    <img src="" alt="compare_logo"/>
+                    <h4>Compare</h4>
+                    <p>Compare student accomodation to find the right home for you.</p>
+                </div>
+                <div className="feature-item">
+                    <img src="" alt="bill_logo"/>
+                    <p>Bills are included in all rent prices.</p>
+                    <p>No hidden fees.</p>
+                </div>
             </div>
         </div>
     </div>
