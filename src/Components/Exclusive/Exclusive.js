3c21a87ea51d8c1bd9b423975e04855d8bcef3a7
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-22 13:43:07 -0700

    import bootstrap icon

--- src/Components/Exclusive/Exclusive.js
+++ src/Components/Exclusive/Exclusive.js
@@ -8,7 +8,7 @@ export default function exclusive () {
             <h3>Compare all inclusive student homes</h3>
             <div className="feature-container">
                 <div className="feature_item">
-                    <img src="" alt="search_logo" />
+                    <i class="bi bi-search-heart"></i>
                     <h4>Search</h4>
                     <p>Find your dream home in the perfect area near your university.</p>
                 </div>
