4f88b492f95d167ccdf9739331f6131f202ac8c6
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-22 11:39:19 -0700

    fetched the picture of city

--- src/Components/CitiList/CitiList.js
+++ src/Components/CitiList/CitiList.js
@@ -1,27 +1,42 @@
 import React from 'react'; 
 import {useState, useEffect } from "react"; 
 import axios from "axios";  
-
-
-
+import CitiCard from '../CitiCard/CitiCard';
+import "./CitiList.css";
 export default function CitiList() {
     const baseUrl = "https://unilife-server.herokuapp.com/cities"; 
-    const [citi, listCities] = useState([]); //set to the initial empty array
+    const [city, setCities] = useState([]); //set to the initial empty array
 
     useEffect(()=>{
-        async function fetchData(){
-            const request = await axios.get(baseUrl); 
-            console.log(request);
-            return request; 
-        }
-        fetchData(); 
+        axios
+        .get(
+            baseUrl
+        )
+        .then((result) => {
+            setCities(result.data.response)
+            // console.log(result); //checking data information after fetching 
+        })
+        .catch((err) => {
+            console.log("error", err); 
+        })
     },[])
-
+    console.log(city);
+    // console.log(city[0].image_url); //checking the data from city variable 
 
   return (
     <div className="list_container">
         <h2>Student Accomodations in our top cities</h2>
         <div className="city_list">
+            {/* <img src={city[0].image_url}/> */}
+            {city?.slice(0, -1).map((citi)=>{   
+                return(
+                <CitiCard 
+                key         = {citi._id}
+                name        = {citi.name}
+                image_url   = {citi.image_url}
+                />
+                )
+            })}
 
         </div>
     </div>
