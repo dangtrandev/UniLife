5e9c166fbd5efda1aae7da87fbf55aca15f5b8c7
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-25 09:14:08 -0700

    insert the icons for social part

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import "./Contacts.css";
-
+import {FaFacebook, FaTwitter, FaInstagram} from "react-icons/fa"; 
 function Contacts() {
   return (
     <div className="contacts_container">
@@ -12,7 +12,10 @@ function Contacts() {
         </div>
 
         <div className="social_container">
-            
+            <h3>Let's Socialize</h3>
+            <div className="social_icons"><FaTwitter /> <p>Twitter</p></div>
+            <div className="social_icons"><FaFacebook /> <p>Facebook</p></div>
+            <div className="social_icons"><FaInstagram /> <p>Instagram</p></div>
         </div>
     </div>
   )
