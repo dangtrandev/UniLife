93d3a9d90013a35e21ca31f004e2fecc3d2b2847
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-25 19:55:53 -0700

    import the footer into the hompage

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import "./Footer.css"; 
+
+
+export default function Footer() {
+  return (
+    <div className="footer_container">
+        <div className="footer_link_container">
+            <a>About us</a>
+            <a>Terms and Condition</a>
+            <a>Privacy & Cookies Polocies</a>
+        </div>
+        <div className="footer_info_container">
+            <p>2022</p>
+            <p>UniLife</p>
+        </div>
+
+    </div>
+  )
+}
