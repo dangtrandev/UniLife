8bfaf1b3704e366ec89c77e4b24713ef09071b80
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-02-28 16:40:58 -0700

    import the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import './App.css';
+
 import NavBar from './Components/NavBar/NavBar'; 
 import Banner from './Components/Banner/Banner';
 import CitiList from './Components/CitiList/CitiList'; 
 import Exclusive from './Components/Exclusive/Exclusive';
 import Selection from './Components/Selection/Selection';
 import Contacts from './Components/Contacts/Contacts';
+import Footer from './Components/Footer/Footer'; 
+
+import Homepage from './Pages/Homepage';
+import HomeDetailsPage from './Pages/HomeDetailsPage'; 
+import CitiDetailsPage from './Pages/CitiDetailsPage';
+import SeeAllCitiPage from './Pages/SeeAllCitiPage'; 
+import { dom } from '@fortawesome/fontawesome-svg-core';
+
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Banner />
-      <CitiList />
-      <Exclusive />
-      <Selection />
-      <Contacts />
+      
 
     </div>
      
