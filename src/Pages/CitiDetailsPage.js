25e45a018f9ffc2020ab32ba8497ceb4afd087b4
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-03-07 12:46:43 -0700

    create citidetailspage.css

--- src/Pages/CitiDetailsPage.js
+++ src/Pages/CitiDetailsPage.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import axios from 'axios'; 
 import Banner from '../Components/Banner/Banner';
 import HomeList from '../Components/HomeList/HomeList';
+import "./CitiDetailsPage.css"; 
 
 export default function CitiDetailsPage() {
   const baseURL = "https://unilife-server.herokuapp.com/cities/"
@@ -35,6 +36,26 @@ export default function CitiDetailsPage() {
       title="Search Accomodation"
       statement="Whatever you're after, we can help you find the right student accomodation for you"
       />
+      <div className="home_search_bar">
+        {/* dropdown button for MinBedroom */}
+        <div className="select_option">
+            <select id="room_type">
+              <option value="">All Room Types</option>
+              <option value="1">Single Room</option>
+              <option value="2">Double Room</option>
+              <option value="3">Suite Room</option>
+            </select>
+        </div>
+        {/* dropdown button for MinBathroom */}
+        <div className="select_option">
+            <select id="bathroom_size">
+              <option value="">All bathroom size</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+        </div>
+      </div>
       <HomeList 
         city = {city}
         propertyCount = {city?.property_count}
