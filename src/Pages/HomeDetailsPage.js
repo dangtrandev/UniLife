396f4ef9756db70d7b0462f0c09b139a3a007bdc
Dang Hoang Huy Tran <developer.dangtran@gmail.com>
2023-03-08 20:31:58 -0700

    solving the usestate issue

--- src/Pages/HomeDetailsPage.js
+++ src/Pages/HomeDetailsPage.js
@@ -1,7 +1,60 @@
-import React from 'react'
+import React from 'react'; 
+import { useState, useEffect } from 'react'; 
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import leftArrow from '../assets/left_arrow.svg'; 
+import './HomeDetailsPage.css'; 
+
+
 
 export default function HomeDetailsPage() {
+  const { homeId } = useParams(); 
+  const [home, setHomes] = useState();
+  const baseURL = "https://unilife-server.herokuapp.com/properties/"
+  console.log(homeId);
+  useEffect(()=>{
+    axios
+    .get(`${baseURL}${homeId}`)
+    .then(
+      (result) =>{
+        setHomes(result.data)
+        console.log(result);//checking the data we get from api
+    })
+    .catch((err)=>console.log(err))
+  },[])
+  
   return (
-    <div>HomeDetailsPage</div>
+    <div className="homeDetailsPage_container">
+      <button className="back_btn">
+        <img src={leftArrow} alt="left arrow" />
+        <p> Back to Search</p>
+      </button>
+
+      <div className="homeDetails_container">
+        {/* spot for the home picture  */}
+        <div className="home_pics">
+          <div className="largest_pics_container">
+             <img src={home?.images[0]}  alt="house image"/>
+          </div>
+          <div className="smaller_pics_container">
+            <img src={home?.images[1]}  alt="house image"/>
+            <img src={home?.images[2]}  alt="house image"/>
+            <img src={home?.images[3]}  alt="house image"/>
+          </div>
+        </div>
+          
+        {/* spot for the home info */}
+        <div className="home_details_info">
+          <div className="home_info">
+                <div className="home_address">
+                  <p>{home?.address.street}, {home?.address.city}, {home?.address.postcode}</p>
+                </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
   )
 }
